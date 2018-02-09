8b0593c78744304990da3cd76d25b400e7d98129
nmariano <mariano.nick@gmail.com>
2018-02-08 22:57:23 -0600

    Add project files.

---
+++ FiveThirtyEight.Data.Interacter/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CsvHelper;
+using System.IO;
+
+namespace FiveThirtyEightData.Interacter
+{
+    public class CollegeMajor
+    {
+        //Major_code,Major,Major_category,Total,Employed,Employed_full_time_year_round,Unemployed,Unemployment_rate,Median,P25th,P75th
+    }
+
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var csvFile = @"c:\users\nick\source\repos\FiveThirtyEight.Data.Interacter\FiveThirtyEight.Data.Interacter\Data\all-ages.csv";
+            var textReader = new StreamReader(csvFile);
+            var csvReader = new CsvReader(textReader);
+            var records = csvReader.GetRecords<CollegeMajor>();
+
+            foreach (CollegeMajor cm in records)
+            {
+                // do things
+            }
+        }
+    }
+}
